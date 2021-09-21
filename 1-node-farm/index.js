7bb71be053ec514bbf7d070bb6987af23f68be1e
grigoar <grigore.nath.j@gmail.com>
2021-09-21 05:57:40 +0300

    first commit

---
+++ 1-node-farm/index.js
@@ -0,0 +1,7 @@
+//We can use modules (like fs) to use in the file
+//we have access to reading/writing to the file system
+//we store the module in the fs variable so we can use later
+const fs = require("fs");
+
+const hello = "Hello world";
+console.log(hello);
