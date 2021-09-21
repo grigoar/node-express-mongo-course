6b5498f91c0651268de9a30ffbc2bc50af725fe3
grigoar <grigore.nath.j@gmail.com>
2021-09-21 06:42:46 +0300

    using the syncronous and asynchrounous priniciples for node.js

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -3,5 +3,32 @@
 //we store the module in the fs variable so we can use later
 const fs = require("fs");
 
-const hello = "Hello world";
-console.log(hello);
+//read from file Synchronous will block the execution
+//Blocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+
+// const textOut = `This is what we know about the avocado: ${textIn}.\n Created on ${Date.now()}`;
+// //write in folder
+// fs.writeFileSync("./txt/input.txt", textOut);
+// console.log("File written");
+
+//Non-blocking, asynchronous way
+//as soon as the reading is done, Node.js will call the callback function we defined(error is usually the first argument)
+//callback -hell but the principle that node works
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("ERROR");
+
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("Your file was been written👌");
+      });
+    });
+  });
+});
+
+console.log("will read file");
