2b0d9955b2172a8986e369b42a273382dcd8edae
grigoar <grigore.nath.j@gmail.com>
2021-09-25 09:51:51 +0300

    using express for crud operations

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 
 const app = express();
 
+//middleware so we can add some data between the request and the response
+app.use(express.json());
+
 //we can use more simplified code with express, but under the hood it is still node code
 // app.get(`/`, (req, res) => {
 //   //   res.status(200).send('Hello from the server side!');
@@ -29,6 +32,91 @@ app.get(`/api/v1/tours`, (req, res) => {
       //   tours: tours,
       tours,
     },
+  });
+});
+
+app.post(`/api/v1/tours`, (req, res) => {
+  // console.log(req.body);
+  //creating a new object with a new id
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+
+  // res.send('done');
+});
+//optional paramters ?
+// app.get(`/api/v1/tours/:id/:x?/:y?`, (req, res) => {
+app.get(`/api/v1/tours/:id`, (req, res) => {
+  console.log(req.params);
+  console.log('-------------------------------');
+  // console.log(tours);
+
+  //convert a string to a number trick
+  const id = req.params.id * 1;
+
+  // const tour = tours.find((el) => el.id === id);
+  const tour = tours.find((el) => {
+    return el.id === id;
+  });
+
+  // if (id > tours.length) {
+  //sol 2
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.patch(`/api/v1/tours/:id`, (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated trour here...>',
+    },
+  });
+});
+
+app.delete(`/api/v1/tours/:id`, (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  //204 no content
+  res.status(204).json({
+    status: 'success',
+    data: null,
   });
 });
 
