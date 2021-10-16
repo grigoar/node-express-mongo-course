b7d943f181a64de8c0d71b1b9b870c4c591aa1ef
grigoar <grigore.nath.j@gmail.com>
2021-10-16 11:48:39 +0300

    added error handling for production

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -22,34 +22,66 @@ const handleValidationErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
+const sendErrorDev = (err, req, res) => {
+  //this is without the root
+  //API
+  if (req.originalUrl.startsWith('/api')) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  }
+  //RENDERED WEBSITE
+  console.log('ERROR 💥💥', err);
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong!',
+    msg: err.message,
   });
 };
 
-const sendErrorProd = (err, res) => {
-  //Operational, trusted error: send message to client
-  //we set this field when we created the error
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
-    // Programmming or other unknown error: don't leak error details
-  } else {
+//This is not working fully so it might need a little more work to do
+//something is happening when we copy the error from send error
+const sendErrorProd = (err, req, res) => {
+  //A. API
+  if (req.originalUrl.startsWith('/api')) {
+    //Operational, trusted error: send message to client
+    //we set this field when we created the error
+    if (err.isOperational) {
+      return res.status(err.statusCode).render('error', {
+        title: 'Something went wrong!',
+        msg: err.message,
+      });
+      // Programmming or other unknown error: don't leak error details
+    }
     //1) Log error
     console.log('ERROR 💥💥', err);
 
     //2) Send generic message
-    res.status(500).json({
-      status: 'Error!',
-      message: 'Something went very wrong',
+    return res.status(err.statusCode).render('error', {
+      title: 'Something went wrong!',
+      msg: 'Please try again later',
     });
   }
+  //B. RENDERED Website
+  //Operational, trusted error: send message to client
+  //we set this field when we created the error
+  if (err.isOperational) {
+    return res.status(err.statusCode).render('error', {
+      title: 'Something went wrong!',
+      msg: err.message,
+    });
+    // Programmming or other unknown error: don't leak error details
+  }
+  //1) Log error
+  console.log('ERROR 💥💥', err);
+
+  //2) Send generic message
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong!',
+    msg: 'Please try again later',
+  });
 };
 
 const handleJWTError = (err) =>
@@ -58,6 +90,7 @@ const handleJWTError = (err) =>
 const handleJWTExpiredError = (err) =>
   new AppError('Your token has expired! Please log in again', 401);
 
+//this don't work fully in production because the copy is not fully populated
 module.exports = (err, req, res, next) => {
   // console.log(err.stack);
 
@@ -65,9 +98,10 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
+    sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === 'production ') {
     let error = { ...err };
+    error.message = err.message;
     //     error.name = err._proto.name;
     error.name = err.name || err.stack.split(':')[0];
     if (error.name === 'CastError') error = handleCastErrorDB(error);
@@ -78,7 +112,7 @@ module.exports = (err, req, res, next) => {
     if (error.name === 'TokenExpiredError')
       error = handleJWTExpiredError(error);
 
-    sendErrorProd(error, res);
+    sendErrorProd(error, req, res);
   }
 };
 
